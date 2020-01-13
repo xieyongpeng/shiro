1928f033a1b11745fe9969fe00b5d794c6a800e6
xieyp <xieyp@asiainfo.com>
2020-01-13 20:40:26 +0800

    class9

---
+++ src/main/java/com/shiro/class9/servlet/IndexServlet.java
@@ -0,0 +1,28 @@
+package com.shiro.class9.servlet;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-29
+ * <p>Version: 1.0
+ */
+@WebServlet(name = "indexServlet", urlPatterns = "/index")
+public class IndexServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Subject subject = SecurityUtils.getSubject();
+        subject.checkRole("admin");
+        req.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(req, resp);
+        
+    }
+}
